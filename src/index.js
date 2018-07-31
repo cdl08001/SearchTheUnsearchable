0c903cc028aed1d8304e765ab968e8d57b4f0370
Calin Lewis <calin.lewis@gmail.com>
2018-07-31 16:53:30 -0400

    Add create-react-app and Bootstrap

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap-theme.css';
+// Will need to add something similar to my componenets:
+// import { Navbar, Jumbotron, Button } from 'react-bootstrap';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
