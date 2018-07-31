a01a5fe9374d94637582936fa522ad0e8d9fefbe
Calin Lewis <calin.lewis@gmail.com>
2018-07-31 17:30:17 -0400

    Add Electron; Remove boilerplate CSS and HTML structure

--- src/App.js
+++ src/App.js
@@ -1,21 +1,19 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+// Enter the following to leverage bootstrap: 
+// import { Grid, Navbar, Jumbotron, Button } from 'react-bootstrap';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <h1>Search The Unsearchable!</h1>
       </div>
     );
   }
 }
 
 export default App;
+
+/*
+
+*/
