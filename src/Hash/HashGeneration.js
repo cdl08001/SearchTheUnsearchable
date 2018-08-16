289ac37818d9e1d3b62587432312c3798f0ce48a
Calin Lewis <calin.lewis@gmail.com>
2018-08-16 14:53:17 -0700

    Add error handling; Pass calculateSHA256 to server

--- src/Hash/HashGeneration.js
+++ src/Hash/HashGeneration.js
@@ -1,18 +1,21 @@
 const crypto = require('crypto');
 const fs = require('fs');
 
-const calculateSHA256 = (file) => {
+const calculateSHA256 = (file, cb) => {
   const hash = crypto.createHash('sha256');
   const input = fs.createReadStream(file.path);
+  input.on('error', err => cb(err));
   input.on('readable', () => {
     const data = input.read();
     if (data) {
       hash.update(data);
     } else {
-      const hashcode = hash.digest('hex');
-      console.log('The hashcode for file: ', file.name, ' is: ', hashcode);
+      const hc = hash.digest('hex');
+      const updatedFileInfo = file;
+      updatedFileInfo.hashcode = hc;
+      return cb(null, updatedFileInfo);
     }
   });
 };
 
-export default calculateSHA256;
+module.exports = calculateSHA256;
--- src/Server/Server.js
+++ src/Server/Server.js
@@ -2,21 +2,30 @@ const express = require('express');
 
 const bodyParser = require('body-parser');
 
+const calculateSHA256 = require('../Hash/HashGeneration');
+
 const app = express();
 
 app.use(bodyParser.json());
 
 app.options('/audio', (req, res) => {
-  console.log('An OPTIONS network request was recieved.');
   res.append('Access-Control-Allow-Origin', 'http://localhost:3000');
   res.append('Access-Control-Allow-Headers', 'Content-Type');
+  console.log('An OPTIONS network request was recieved.');
   res.status(200).send('Sure.');
 });
 
 app.post('/audio', (req, res) => {
   console.log('An audio file has been recieved');
-  console.log('The request body is: ', req.body);
   res.append('Access-Control-Allow-Origin', 'http://localhost:3000');
+  req.body.audioFiles.forEach((file) => {
+    calculateSHA256(file, (err, data) => {
+      if (err) {
+        throw err;
+      }
+      console.log('File Info with Hashcode: ', data);
+    });
+  });
   res.send('Here is your response');
 });
 
