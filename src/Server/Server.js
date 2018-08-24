587d1f08d7a7746ce6254ed5a88be4b372ff63d9
Calin Lewis <calin.lewis@gmail.com>
2018-08-24 12:47:46 -0700

    Convert calculateSHA256 to promise and adjust server

--- src/Server/Server.js
+++ src/Server/Server.js
@@ -21,35 +21,45 @@ app.options('/audio', (req, res) => {
 
 // Current configuration has node creating two readStreams.
 // This should be reduced to one stream in the near future.
+// app.post('/audio', (req, res) => {
+//   console.log('An audio file has been recieved');
+//   res.append('Access-Control-Allow-Origin', 'http://localhost:3000');
+//   req.body.audioFiles.forEach((file) => {
+//     calculateSHA256(file, (hashError, hashResultsData) => {
+//       if (hashError) { throw hashError; }
+//       console.log('Success: Hashcode Generated: ', hashResultsData);
+//       uploadAudio(file, (uploadError, uploadResultsData) => {
+//         if (uploadError) throw uploadError;
+//         console.log(`Success: Uploaded ${file.name} to S3!: ${uploadResultsData}`);
+//         submitTranscriptionJob(file, (submitTranscriptionError, submitTranscribeData, jobId) => {
+//           if (submitTranscriptionError) throw submitTranscriptionError;
+//           console.log('Success: Transcription Job Submitted: ', submitTranscribeData);
+//           checkTranscriptionStatus(jobId, (transcriptionError, failureReason, inProgress, data) => {
+//             if (transcriptionError) {
+//               throw transcriptionError;
+//             } else if (failureReason) {
+//               console.log('Error: Transcription Failed: ', failureReason);
+//             } else if (inProgress) {
+//               console.log('The job is still in progress. Will try again in 30 seconds.');
+//             } else if (data) {
+//               console.log('Success: Transcription Complete', data);
+//             }
+//           });
+//         });
+//       });
+//     });
+//   });
+//   res.send('Here is your response');
+// });
 app.post('/audio', (req, res) => {
   console.log('An audio file has been recieved');
   res.append('Access-Control-Allow-Origin', 'http://localhost:3000');
   req.body.audioFiles.forEach((file) => {
-    calculateSHA256(file, (hashError, hashResultsData) => {
-      if (hashError) { throw hashError; }
-      console.log('Success: Hashcode Generated: ', hashResultsData);
-      uploadAudio(file, (uploadError, uploadResultsData) => {
-        if (uploadError) throw uploadError;
-        console.log(`Success: Uploaded ${file.name} to S3!: ${uploadResultsData}`);
-        submitTranscriptionJob(file, (submitTranscriptionError, submitTranscribeData, jobId) => {
-          if (submitTranscriptionError) throw submitTranscriptionError;
-          console.log('Success: Transcription Job Submitted: ', submitTranscribeData);
-          checkTranscriptionStatus(jobId, (transcriptionError, failureReason, inProgress, data) => {
-            if (transcriptionError) {
-              throw transcriptionError;
-            } else if (failureReason) {
-              console.log('Error: Transcription Failed: ', failureReason);
-            } else if (inProgress) {
-              console.log('The job is still in progress. Will try again in 30 seconds.');
-            } else if (data) {
-              console.log('Success: Transcription Complete', data);
-            }
-          });
-        });
-      });
-    });
+    calculateSHA256(file)
+      .then(hashData => console.log('SUCCESS: Hashcode Generated:', hashData))
+      .then(() => console.log('poop'))
+      .catch(error => console.log('ERROR: ', error));
   });
-  res.send('Here is your response');
 });
 
 app.listen(3001, () => console.log('Server is listening on port 3001!'));
