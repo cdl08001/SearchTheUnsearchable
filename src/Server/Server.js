409f945159c9d079f35bbd41f1e596f4b0c81978
Calin Lewis <calin.lewis@gmail.com>
2018-08-27 21:26:27 -0700

    Decoupling of S3 and Transcribe functions from single route handler
    For purposes of separations of concerns I removed the S3 and Transcribe
    functions from the single '/audio' route handler and broke it out into
    several route handler functions. This will require more manual user
    input on the client side, however it will allow for easier testing and
    more predictable behavior. Automation will come at a future time.

--- src/Server/Server.js
+++ src/Server/Server.js
@@ -20,21 +20,62 @@ app.options('/audio', (req, res) => {
   res.append('Access-Control-Allow-Origin', 'http://localhost:3000');
   res.append('Access-Control-Allow-Headers', 'Content-Type');
   console.log('An OPTIONS network request was recieved.');
-  res.status(200).send('Sure.');
+  res.status(200).send();
 });
 
+// Calculate hashcode and send back to client
+// Client should restrict the number of selected files to 1 until handling of
+// multiple files can be supported
 app.post('/audio', (req, res) => {
   console.log('An audio file has been recieved');
   res.append('Access-Control-Allow-Origin', 'http://localhost:3000');
   req.body.audioFiles.forEach((file) => {
     calculateSHA256(file)
-      .then(hashFileData => uploadAudio(hashFileData))
-      .then(uploadFileData => submitTranscriptionJob(uploadFileData))
-      .then(transcriptionJobData => checkTranscriptionStatus(transcriptionJobData))
-      .then(transcriptionJobResults => pullTranscription(transcriptionJobResults))
-      .then(transcriptionResults => res.status(200).send(transcriptionResults))
-      .catch(error => res.status(500).console.log('ERROR: ', error));
+      .then((hashFileData) => {
+        res.status(200).send(hashFileData);
+      })
+      .catch(hashError => res.status(500).send('ERROR: Hash Calculation Error: ', hashError));
   });
+});
+
+app.post('/audio', (req, res, next) => {
+  uploadAudio(res.locals.hashFileData)
+    .then((uploadFileData) => {
+      res.locals.uploadFileData = uploadFileData;
+      next();
+    })
+    .catch(uploadFileError => res.status(500).send('ERROR: File Upload Error: ', uploadFileError));
+});
+
+app.post('/audio', (req, res, next) => {
+  submitTranscriptionJob(res.locals.uploadFileData)
+    .then((transcriptionJobData) => {
+      res.locals.transcriptionJobData = transcriptionJobData;
+      next();
+    })
+    .catch(submitTranscriptionJobError => res.status(500).send('ERROR: Job Submission Error: ', submitTranscriptionJobError));
+});
+
+app.post('/audio', (req, res, next) => {
+  checkTranscriptionStatus(res.locals.transcriptionJobData)
+    .then((transcriptionStatusData) => {
+      res.locals.transcriptionStatusData = transcriptionStatusData;
+      next();
+    })
+    .catch(checkTranscriptionStatusError => res.status(500).send('ERROR: Check Transcription Status Error: ', checkTranscriptionStatusError));
+});
+
+app.post('/audio', (req, res, next) => {
+  pullTranscription(res.locals.transcriptionStatusData)
+    .then((transcriptionResults) => {
+      res.locals.transcriptionResults = transcriptionResults;
+      next();
+    })
+    .catch(pullTranscriptionError => res.status(500).send('ERROR: Pull Transcription Error: ', pullTranscriptionError));
+});
+
+app.post('/audio', (req, res) => {
+  res.send(500).send(res.locals.transcriptionResults);
 });
 
 app.listen(3001, () => console.log('Server is listening on port 3001!'));
