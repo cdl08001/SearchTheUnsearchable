18bc33319f432c5080483829881b5c7678bfb5ba
Calin Lewis <calin.lewis@gmail.com>
2018-09-04 18:27:38 -0700

    Install MongoDB; Add basic db connection functionality

---
+++ src/Database/Database.js
@@ -0,0 +1,18 @@
+const { MongoClient } = require('mongodb');
+const assert = require('assert');
+
+// Connection URL
+const url = 'mongodb://localhost:27017';
+
+// Database Name
+const dbName = 'SearchTheUnsearchable';
+
+// Use connect method to connect to the server
+MongoClient.connect(url, (err, client) => {
+  assert.equal(null, err);
+  console.log('Connected successfully to server');
+
+  const db = client.db(dbName);
+
+  client.close();
+});
